d8a7c644928c573671ae6ec6b8aee110fb9a0d61
Leonardo <leonardo@Robinsons-Mac-mini.local>
2019-10-25 12:25:46 -0300

    Add react-router, animation, readme, fix structure

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { BrowserRouter } from 'react-router-dom';
 
+const app = (
+    <BrowserRouter basename="/">
+        <App />
+    </BrowserRouter>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
